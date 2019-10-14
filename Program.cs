38a51571a7646d94c4de449714d508af57a35b59
Heraved <48395159+Heraved@users.noreply.github.com>
2019-10-14 09:12:20 +0200

    DiaryApp

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiaryApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            MenuDiary.Menu();
+
+            
+
+            /*
+            string name1;
+            name1 = Console.ReadLine();
+
+            Console.WriteLine("Wprowadź ocene:");
+            float assessment = new float();
+            assessment = float.Parse(Console.ReadLine());    /// konwertowanie float na string --> float.Parse
+
+
+            var student1 = name1 + "\t" + assessment;
+
+            List<string> ListOfStudents = new List<string>();
+
+          
+            */
+            
+
+            Console.ReadKey();
+        }
+    }
+}
