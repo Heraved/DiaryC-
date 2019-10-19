cd480a29cf59fe1bbda6701f4f368d80e12ac794
Heraved <48395159+Heraved@users.noreply.github.com>
2019-10-19 19:23:29 +0200

    Add files via upload

--- Student.cs
+++ Student.cs
@@ -8,29 +8,36 @@ namespace DiaryApp2
 {
     public class Student
     {
-        public static string nameStudent;
-        public static int assessment;
+        public static string name;
+        public static string surname;
+        public static int age;
+        public static int grades;
 
-       
 
-        public static void studentInput()
+
+        public static void info()
         {
-            Console.WriteLine("Wprowadź dane studenta:");
-            nameStudent = Console.ReadLine();
+            Console.WriteLine("Wprowadź imie studenta:");
+            name = Console.ReadLine();
+            Console.WriteLine("Wprowadź nazwisko studenta:");
+            surname = Console.ReadLine();
+            Console.WriteLine("Podaj wiek studenta:");
+            age = Convert.ToInt32(Console.ReadLine());
 
-           
         }
-        public static void studentAssessment()
+        public static void studentgrades()
         {
-            Console.WriteLine("Wprowadź oceny studenta:");
-            assessment = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Wprowadź oceny studenta:, ");
+            grades = Convert.ToInt32(Console.ReadLine());
+
         }
         public static void showStudent()
         {
-            
-            Console.WriteLine(nameStudent + "\t" + assessment);
-            Console.ReadKey();
-            
+            List <Student> ListofStudents = new List<Student>();
+            ListofStudents.Add(name);
+            ListofStudents.Add(surname);
+            ListofStudents.Add(age);
         }
     }
 }
+   
